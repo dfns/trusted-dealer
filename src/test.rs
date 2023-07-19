adbac3ac8c698195ba3bcf1dae2fac49e7badd13
d86leader <nikita@dfns.co>
2023-07-19 12:29:04 +0200

    remove openssl, compile to wasm

--- src/test.rs
+++ src/test.rs
@@ -20,24 +20,6 @@ fn read_share(bytes: &[u8]) -> cggmp21::key_share::IncompleteKeyShare<Secp256k1>
     .try_into().unwrap()
 }
 
-struct DecryptionKey {
-    key: openssl::pkey::PKey<openssl::pkey::Private>,
-    cert: openssl::x509::X509,
-}
-
-impl DecryptionKey {
-    fn from_bytes(key_bytes: &[u8], cert_bytes: &[u8]) -> Self {
-        let cert = openssl::x509::X509::from_pem(&cert_bytes).unwrap();
-        let key = openssl::pkey::PKey::private_key_from_pem(key_bytes).unwrap();
-        Self { key, cert }
-    }
-
-    fn decrypt(&self, input: &[u8]) -> Vec<u8> {
-        let bundle = openssl::pkcs7::Pkcs7::from_pem(input).unwrap();
-        bundle.decrypt(&self.key, &self.cert, openssl::pkcs7::Pkcs7Flags::empty()).unwrap()
-    }
-}
-
 #[test]
 fn shard_and_restore() {
     let mut rng = rand_core::OsRng;
@@ -47,17 +29,4 @@ fn shard_and_restore() {
 
     let secret = cggmp21::key_share::reconstruct_secret_key(&shares).unwrap();
     assert_eq!(secret_share.as_ref(), secret.as_ref());
-}
-
-#[test]
-fn encrypt_decrypt() {
-    let cert_bytes = include_bytes!("../test_data/cert.pem");
-    let key_bytes = include_bytes!("../test_data/key.pem");
-    let enc = super::EncryptionKey::from_bytes(cert_bytes).unwrap();
-    let dec = DecryptionKey::from_bytes(key_bytes, cert_bytes);
-
-    let data = "hello openssl world";
-    let ciphertext = enc.encrypt(data.as_bytes()).unwrap();
-    let deciphered = dec.decrypt(&ciphertext);
-    assert_eq!(data.as_bytes(), &deciphered);
 }
