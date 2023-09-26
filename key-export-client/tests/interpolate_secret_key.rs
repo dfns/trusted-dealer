1899d9074a428bf2c08a0fa842798e45aab69d51
Orestis Alpos <orestis@dfns.co>
2023-09-26 14:28:48 +0200

    remove unnecessary import

--- key-export-client/tests/interpolate_secret_key.rs
+++ key-export-client/tests/interpolate_secret_key.rs
@@ -11,14 +11,29 @@ fn get_random_keys_and_shares<E: Curve>(
     let secret_key = SecretScalar::<E>::random(&mut rng);
     let public_key = Point::generator() * &secret_key;
 
-    let secret_shares =
-        dfns_key_import_common::split_secret_key(&mut rng, t, n, &secret_key).unwrap();
+    let secret_shares = {
+        let key_shares_indexes = (1..=n)
+            .map(|i| NonZero::from_scalar(Scalar::from(i)))
+            .collect::<Option<Vec<_>>>()
+            .unwrap();
+        let f = generic_ec_zkp::polynomial::Polynomial::sample_with_const_term(
+            &mut rng,
+            usize::from(t) - 1,
+            secret_key.clone(),
+        );
+        let secret_shares = key_shares_indexes
+            .iter()
+            .map(|i| f.value(i))
+            .map(|mut x| SecretScalar::new(&mut x))
+            .collect::<Vec<_>>();
+        secret_shares
+    };
 
     let shares = (1..n + 1)
         .zip(secret_shares)
         .map(|(i, s)| KeySharePlaintext::<E> {
             index: NonZero::from_scalar(Scalar::from(i)).unwrap(),
-            secret_share: s.secret_share.clone(),
+            secret_share: s,
         })
         .collect::<Vec<KeySharePlaintext<E>>>();
 
