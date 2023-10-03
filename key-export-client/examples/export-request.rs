208eb9475d537f4db14b374e141cd1842507a2c2
Orestis Alpos <orestis@dfns.co>
2023-10-03 16:50:41 +0200

    example print request

--- key-export-client/examples/export-request.rs
+++ key-export-client/examples/export-request.rs
@@ -7,5 +7,5 @@ fn main() {
 fn print_export_request() {
     let ctx = KeyExportContext::new().map_err(|_| {}).unwrap();
     let req = ctx.build_key_export_request().map_err(|_| {}).unwrap();
-    println!("{:?}", req);
+    println!("{:?}", serde_json::to_string(&req).unwrap());
 }
