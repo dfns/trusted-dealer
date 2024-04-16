08292a059dd506c1393968314ca73ad95f0c3282
Denis Varlakov <denis@dfns.co>
2024-04-16 10:46:50 +0200

    Change Error type once again

--- common/src/error.rs
+++ common/src/error.rs
@@ -6,13 +6,11 @@
 use core::fmt;
 
 /// Error type
-#[cfg(not(target_arch = "wasm32"))]
 #[derive(Debug)]
 pub struct Error {
     desc: alloc::string::String,
 }
 
-#[cfg(not(target_arch = "wasm32"))]
 impl Error {
     /// Constructs an error
     pub fn new(desc: &str) -> Self {
@@ -20,36 +18,16 @@ impl Error {
     }
 }
 
-#[cfg(not(target_arch = "wasm32"))]
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(&self.desc)
     }
 }
 
-/// Error type
-#[cfg(target_arch = "wasm32")]
-pub struct Error(wasm_bindgen::JsError);
-
-#[cfg(target_arch = "wasm32")]
-impl Error {
-    /// Constructs an error
-    pub fn new(desc: &str) -> Self {
-        Self(wasm_bindgen::JsError::new(desc))
-    }
-}
-
-#[cfg(target_arch = "wasm32")]
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("JsError")
-    }
-}
-
 #[cfg(target_arch = "wasm32")]
 impl From<Error> for wasm_bindgen::JsValue {
     fn from(err: Error) -> Self {
-        err.0.into()
+        wasm_bindgen::JsError::new(&err.desc).into()
     }
 }
 
